f0bbb79db90b2e7007e01aa760a7749e2e0d5604
J <83779137+JakeHourihan@users.noreply.github.com>
2023-08-31 14:42:01 +0000

    added description of software at top of main

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
+"""This mathematical model uses modified DH parameters that reference the 
+previous joint axis i-1 as opossed to traditional models that reference i.
+This affects how the table is filled and incorrectly doing so will negatively impact 
+the kinematic calculations"""
+
 import math
 import numpy as numpy
 
-a = input("Please enter a number: ")
-a = int(a)
+dhTable = np.array[]
 
-print(a)
 
-#this is a change
+
+numJoints = input('How many joints does your robot have? ')
+numJoints = int(numJoints)
+
+for joint in numJoints:
+    ai1
+    theta
+    d1
+    
